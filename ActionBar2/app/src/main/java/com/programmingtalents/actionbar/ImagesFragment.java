ba320a6e7e8a3900f68be1ca498af9f07d641f71
Amit Saxena <Ravinder.Kundarapu@ncr.com>
2016-12-27 11:44:12 +0530

    ActionBAr with tabs

---
+++ ActionBar2/app/src/main/java/com/programmingtalents/actionbar/ImagesFragment.java
@@ -0,0 +1,21 @@
+package com.programmingtalents.actionbar;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.acadgild.actionbar.R;
+
+public class ImagesFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_images, container, false);
+		
+		return rootView;
+	}
+}
